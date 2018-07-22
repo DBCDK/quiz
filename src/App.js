ede5ec7a5e3b64dfdcd421f5b9a52c085ecdba7a
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2018-07-22 10:59:05 +0200

    Progress on #1 and #2
    - added prettier
    - started notes on backend data design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,60 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
 
+async function main() {
+  let result;
+  const op = window.dbcOpenPlatform;
+
+  const [type_t] = await op.storage({
+    find: {_owner: 'openplatform', name: 'type'}
+  });
+
+  try {
+    result = await op.storage({find: {_owner: 'openplatform', name: 'type'}});
+    console.log(0, result);
+
+    result = await op.storage({get: result[0]});
+    console.log(1, result);
+
+    result = await op.storage({
+      put: {
+        _type: type_t,
+        name: 'quiz',
+        contenttype: 'application/json',
+        charset: 'utf-8',
+        permissions: {
+          read: 'any'
+        },
+        indexes: [{type: 'id', keys: ['tags']}]
+      }
+    });
+    console.log(2, result);
+    const quiz_t = result._id;
+
+    let o = {
+      _type: quiz_t,
+      tags: ['foo', 'bar', 'baz']
+    };
+    result = await op.storage({put: o});
+    console.log('o', o);
+    console.log(3, result);
+
+    Object.assign(o, result);
+    o.tags = ['foo', 'quux'];
+    console.log('o', o);
+
+    result = await op.storage({put: o});
+    console.log(4, result);
+  } catch (e) {
+    console.log(e);
+  }
+  /*
+  result = await window.dbcOpenPlatform.storage({put: {
+      _type: 'openplatform.type',
+      id: 'openplatform.type'
+      }});
+      */
+}
+main();
 class App extends Component {
   render() {
     return (
