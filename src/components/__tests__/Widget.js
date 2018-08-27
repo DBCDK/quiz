eea30d23f1586206a16d7d06be8f3f57a635830b
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2018-08-27 12:14:22 +0200

    tests, fixes #77

--- src/components/__tests__/Widget.js
+++ src/components/__tests__/Widget.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Widget, mapStateToProps} from '../Widget';
+import {Widget, mapStateToProps, mapDispatchToProps} from '../Widget';
 import renderer from 'react-test-renderer';
 import sampleState from '../../redux/sampleState';
+import Immutable from 'immutable';
 
 it('maps state to props', () => {
   expect(mapStateToProps(sampleState)).toMatchSnapshot();
+});
+it('maps dispatch to props', () => {
+  expect(mapDispatchToProps(() => {})).toMatchSnapshot();
 });
 it('renders', () => {
   const tree = renderer.create(<Widget quizTitle="Titel for Quiz" />).toJSON();
   expect(tree).toMatchSnapshot();
 });
 it('renders page', () => {
-  const page = [
+  const page = Immutable.fromJS([
     {type: 'text', text: 'hello'},
     {
       type: 'buttonGroup',
       ui: [{type: 'button', text: 'but'}]
     },
     {type: 'image', image: 'some://url'}
-  ];
-  const tree = renderer.create(<Widget quizTitle="Titel for Quiz" />).toJSON();
+  ]);
+  const tree = renderer
+    .create(
+      <Widget
+        quizTitle="Titel for Quiz"
+        ui={page}
+        vars={Immutable.fromJS({})}
+      />
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
