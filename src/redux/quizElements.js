6cb67d010c64d4799b75db4ba9197a58817b80d7
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2018-09-05 14:55:21 +0200

    Make reordering of quiz sections work, fixes #99, fixes #58.

--- src/redux/quizElements.js
+++ src/redux/quizElements.js
@@ -26,22 +26,26 @@ export function addSection(quiz, {screenId, screens, before}) {
   return quiz;
 }
 
+function sectionScreens(quiz, sectionId) {
+  const screens = [sectionId];
+  let count;
+  do {
+    count = screens.length;
+    for (const [screenId, screen] of quiz.get('screens')) {
+      const parent = screen.get('parent');
+      if (screens.includes(parent) && !screens.includes(screenId)) {
+        screens.push(screenId);
+      }
+    }
+  } while (count !== screens.length);
+  return screens;
+}
+
 export function deleteSection(quiz, id) {
   let screens = quiz.get('screens');
   const next = screens.getIn([id, 'nextSection']);
 
-  const toDelete = [id];
-  let deleteCount;
-  do {
-    deleteCount = toDelete.length;
-    for (const [screenId, screen] of screens) {
-      const parent = screen.get('parent');
-      if (toDelete.includes(parent) && !toDelete.includes(screenId)) {
-        toDelete.push(screenId);
-      }
-    }
-  } while (deleteCount !== toDelete.length);
-
+  const toDelete = sectionScreens(quiz, id);
   screens = screens.filter((screen, screenId) => !toDelete.includes(screenId));
   quiz = quiz.set('screens', screens);
 
@@ -61,9 +65,25 @@ export function findScreenActions(o, acc = []) {
 
 export function moveSection(state, action) {
   const {screens, from, to} = action;
+  if (
+    (typeof to === 'number' && from == screens.length - 1) ||
+    to == screens.length - 1
+  ) {
+    return state;
+  }
   const movedElem = screens[from];
-  const insertAfterId = screens[to - (from > to ? 1 : 0)];
-  // deleteScreen(quiz, movedElem)
-  // insertAfter({quiz,insertAfterId, , })
-  return state;
+  const insertBeforeId = screens[(to + (from > to ? 0 : 1)) % screens.length];
+  let movedScreens = sectionScreens(state.get('quiz'), movedElem);
+  movedScreens = movedScreens.map(id => [
+    id,
+    state.getIn(['quiz', 'screens', id])
+  ]);
+  movedScreens = new Immutable.Map(movedScreens);
+  let quiz = deleteSection(state.get('quiz'), movedElem);
+  quiz = addSection(quiz, {
+    before: insertBeforeId,
+    screenId: movedElem,
+    screens: movedScreens
+  });
+  return state.set('quiz', quiz);
 }
