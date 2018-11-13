baaa4b98c0124d15002399560f4dfc0d8a9c8de2
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2018-11-13 09:35:04 +0100

    Updates

---
+++ precache-manifest.a7d2fcd774bae716379768f7b4324c5b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3068285a94e79392a7d0",
+    "url": "/quiz/static/js/runtime~main.3068285a.js"
+  },
+  {
+    "revision": "7714ebc6531df0f95741",
+    "url": "/quiz/static/js/main.7714ebc6.chunk.js"
+  },
+  {
+    "revision": "dbfcf233fdcebc97c741",
+    "url": "/quiz/static/js/1.dbfcf233.chunk.js"
+  },
+  {
+    "revision": "bad3e239974998cc02415a40ff41deaa",
+    "url": "/quiz/index.html"
+  }
+];
