04b8c6b49f12302137bc9bb1b3777fd09cc09910
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2018-11-21 11:23:22 +0100

    refactor, fixes #238

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -5,12 +5,89 @@ import {
   infoScreenData,
   answerButtonData
 } from '../quizData';
-import {moveSection, deleteSection, addSection} from './quizElements';
 import Immutable from 'immutable';
 import uuidv4 from 'uuid/v4';
+import assert from 'assert';
 
 const initialState = sampleState;
 
+function deepReplace(o, from, to) {
+  if (o.map) {
+    return o.map(v => deepReplace(v, from, to));
+  }
+  if (o === from) {
+    return to;
+  }
+  return o;
+}
+function sectionScreens(quiz, sectionId) {
+  const screens = [sectionId];
+  let count;
+  do {
+    count = screens.length;
+    for (const [screenId, screen] of quiz.get('screens')) {
+      const parent = screen.get('parent');
+      if (screens.includes(parent) && !screens.includes(screenId)) {
+        screens.push(screenId);
+      }
+    }
+  } while (count !== screens.length);
+  return screens;
+}
+function addSection(quiz, {screenId, screens, before}) {
+  function findScreenActions(o, acc = []) {
+    if (Immutable.isCollection(o)) {
+      const screen = o.getIn(['action', 'screen']);
+      if (screen) {
+        acc.push(screen);
+      }
+      o.forEach(v => findScreenActions(v, acc));
+    }
+    return acc;
+  }
+  screens = Immutable.fromJS(screens);
+
+  let needsReplace = Immutable.Set([quiz.get('start')]);
+  let prevSet;
+  do {
+    prevSet = needsReplace;
+    /* eslint-disable no-loop-func */
+    prevSet.forEach(screenId => {
+      const children = findScreenActions(quiz.getIn(['screens', screenId]));
+      children.forEach(child => {
+        if (child !== before) {
+          needsReplace = needsReplace.add(child);
+        }
+      });
+    });
+  } while (!prevSet.equals(needsReplace));
+
+  needsReplace.forEach(replaceInId => {
+    quiz = quiz.updateIn(['screens', replaceInId], screen =>
+      deepReplace(screen, before, screenId)
+    );
+  });
+
+  assert(screens.getIn([screenId, 'nextSection']));
+  screens = deepReplace(
+    screens,
+    screens.getIn([screenId, 'nextSection'], before),
+    before
+  );
+  quiz = quiz.set('screens', quiz.get('screens').merge(screens));
+
+  return quiz;
+}
+function deleteSection(quiz, id) {
+  let screens = quiz.get('screens');
+  const next = screens.getIn([id, 'nextSection']);
+
+  const toDelete = sectionScreens(quiz, id);
+  screens = screens.filter((screen, screenId) => !toDelete.includes(screenId));
+  quiz = quiz.set('screens', screens);
+
+  return deepReplace(quiz, id, next);
+}
 function pageAction(state, action) {
   let {screen, increment, set, callback} = action.action;
 
@@ -189,7 +266,30 @@ export function root(state = initialState, action) {
       return state.update('quiz', quiz => deleteSection(quiz, action.screenId));
     }
     case 'ADMIN_MOVE_SECTION': {
-      return moveSection(state, action);
+      const {screens, from, to} = action;
+      if (
+        (typeof to === 'number' && from === screens.length - 1) ||
+        to === screens.length - 1 ||
+        to === 0
+      ) {
+        return state;
+      }
+      const movedElem = screens[from];
+      const insertBeforeId =
+        screens[(to + (from > to ? 0 : 1)) % screens.length];
+      let movedScreens = sectionScreens(state.get('quiz'), movedElem);
+      movedScreens = movedScreens.map(id => [
+        id,
+        state.getIn(['quiz', 'screens', id])
+      ]);
+      movedScreens = new Immutable.Map(movedScreens);
+      let quiz = deleteSection(state.get('quiz'), movedElem);
+      quiz = addSection(quiz, {
+        before: insertBeforeId,
+        screenId: movedElem,
+        screens: movedScreens
+      });
+      return state.set('quiz', quiz);
     }
     case 'ADD_QUESTION_ANSWER': {
       const nextScreen = state.getIn([
